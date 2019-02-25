7d9a64b6cc68304d6f37a69c603e02ba94e36dfe
kaushalprateek <pkausha4@my.centennialcollege.ca>
2019-02-25 05:02:36 -0500

    edit button updated

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 module.exports = {
   //local MongoDB deployment ->
-  //URI: "mongodb://localhost/books308"
+  URI: "mongodb://localhost/books308"
   //Remote MongoDB deployment -> let
-  URI: "mongodb://kaushal:12345a@ds064278.mlab.com:64278/books308"
+  //URI: "mongodb://kaushal:12345a@ds064278.mlab.com:64278/books308"
 };
--- server/routes/books.js
+++ server/routes/books.js
@@ -4,12 +4,12 @@ let router = express.Router();
 let mongoose = require("mongoose");
 
 // define the book model
-let book = require("../models/books");
+let bookModel = require("../models/books");
 
 /* GET books List page. READ */
 router.get("/", (req, res, next) => {
   // find all books in the books collection
-  book.find((err, books) => {
+  bookModel.find((err, books) => {
     if (err) {
       return console.error(err);
     } else {
@@ -36,17 +36,44 @@ router.post("/add", (req, res, next) => {
 });
 
 // GET the Book Details page in order to edit an existing Book
-router.get("/:id", (req, res, next) => {
-  /*****************
-   * ADD CODE HERE *
-   *****************/
+router.get("/details/:id", (req, res, next) => {
+  let id = req.params.id;
+  console.log(id);
+  bookModel.findById(id, (err, bookObject) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    } else {
+      // show the edit view
+      res.render("books/details", {
+        title: "Edit books",
+        books: bookObject
+      });
+    }
+  });
 });
 
 // POST - process the information passed from the details form and update the document
-router.post("/:id", (req, res, next) => {
-  /*****************
-   * ADD CODE HERE *
-   *****************/
+router.post("/details/:id", (req, res, next) => {
+  let id = req.params.id;
+
+  let updatedBook = bookModel({
+    _id: id,
+    Title: req.body.title,
+    Price: req.body.price,
+    Author: req.body.author,
+    Genre: req.body.genre
+  });
+
+  bookModel.update({ _id: id }, updatedBook, err => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    } else {
+      // refresh the book-list
+      res.redirect("/books");
+    }
+  });
 });
 
 // GET - process the delete by user id
